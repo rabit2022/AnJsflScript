c3a35b52420b7ba4604066494df6fa4a3c1a2b1f
rabit2022 <3101829204@qq.com>
2025-07-26 14:48:35 +0800

    0726    生成环绕飞行_内部.ts  完成 #06.环绕飞行.ts   未完成

--- lib/03.打斗/06.环绕飞行/生成环绕飞行_内部.generated.jsfl
+++ lib/03.打斗/06.环绕飞行/生成环绕飞行_内部.generated.jsfl
@@ -1,7 +1,8 @@
 // 这个文件由脚本 生成环绕飞行_内部.ts 自动生成，任何手动修改都将会被覆盖.
-require(["require", "_exports", "checkUtil", "SymbolNameGenerator", "SAT"], function (require, exports, checkUtil_1, SymbolNameGenerator_1, SAT_1) {
+require(["require", "_exports", "checkUtil", "SymbolNameGenerator", "SAT", "JSFLConstants", "KeyFrameOperation", "EaseCurve", "ElementAnim", "loglevel"], function (require, exports, checkUtil_1, SymbolNameGenerator_1, SAT_1, JSFLConstants, KeyFrameOperation_1, EaseCurve_1, ElementAnim_1, log) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    var _a = JSFLConstants.Numerics.frame.frameList, FRAME_1 = _a.FRAME_1, FRAME_15 = _a.FRAME_15, FRAME_30 = _a.FRAME_30;
     var doc = fl.getDocumentDOM();
     if (!(0, checkUtil_1.CheckDom)(doc)) {
         return;
@@ -19,26 +20,62 @@ require(["require", "_exports", "checkUtil", "SymbolNameGenerator", "SAT"], func
         return;
     }
     function getRect(radius) {
-        var elementPos = SAT_1.Vector.from(selection[0]);
         var size = new SAT_1.Size(radius * 2, radius * 0.8);
         var center = new SAT_1.Vector(size.width / 2, 0);
         var rect = SAT_1.Rectangle.fromCenter(center, size);
-        return rect.addOffset(elementPos);
+        return rect;
     }
+    function getOffsets(radius) {
+        var rect = getRect(radius);
+        var rightCenter = rect.getCorner("right center");
+        var leftCenter = rect.getCorner("left center");
+        var offset = new SAT_1.Vector(0, 2);
+        var top = leftCenter.clone().sub(offset);
+        var bottom = leftCenter.clone().add(offset);
+        log.info("环绕矩形", top, bottom);
+        var offsets = [top, rightCenter, bottom];
+        return offsets;
+    }
+    var KEY_FRAMES = [FRAME_1, FRAME_15, FRAME_30];
+    var MOTION_GUIDE_LAYER_INDEX = 0;
+    var AROUND_LAYER_INDEX = 1;
     function Main() {
+        var layers;
         var RADIUS = 400;
         doc.enterEditMode("inPlace");
         var timeline = doc.getTimeline();
         var symbolName = (0, SymbolNameGenerator_1.generateNameUseLast)("环绕飞行_飞行物");
         doc.convertToSymbol("graphic", symbolName, "center");
         {
-            var motionGuideLayerIndex = timeline.addMotionGuide();
-            var layers = timeline.layers;
-            var motionGuideLayer = layers[motionGuideLayerIndex];
+            MOTION_GUIDE_LAYER_INDEX = timeline.addMotionGuide();
+            layers = timeline.layers;
+            var motionGuideLayer = layers[MOTION_GUIDE_LAYER_INDEX];
             motionGuideLayer.name = "运动曲线";
             var rect = getRect(RADIUS);
-            doc.addNewOval(rect, true);
+            var elementPos = SAT_1.Vector.from(selection[0]);
+            var finalRect = rect.addOffset(elementPos);
+            doc.addNewOval(finalRect, true);
         }
+        timeline.insertFrames(FRAME_30, true);
+        timeline.setSelectedLayers(AROUND_LAYER_INDEX);
+        (0, KeyFrameOperation_1.convertToKeyframesSafety)(timeline, KEY_FRAMES);
+        {
+            layers = timeline.layers;
+            var aroundLayer = layers[AROUND_LAYER_INDEX];
+            var offsets_1 = getOffsets(RADIUS);
+            function setPosition(frame, position) {
+                timeline.setSelectedFrames([AROUND_LAYER_INDEX, frame, frame + 1]);
+                var FRAME_1_Element = aroundLayer.frames[frame].elements[0];
+                FRAME_1_Element.x = position.x;
+                FRAME_1_Element.y = position.y;
+            }
+            KEY_FRAMES.forEach(function (frame, index) {
+                setPosition(frame, offsets_1[index]);
+            });
+            (0, EaseCurve_1.setEaseCurveEx)(timeline, KEY_FRAMES, "Classic Ease");
+        }
+        doc.exitEditMode();
+        (0, ElementAnim_1.playLoop)();
     }
     Main();
 });
--- lib/03.打斗/06.环绕飞行/生成环绕飞行_内部.ts
+++ lib/03.打斗/06.环绕飞行/生成环绕飞行_内部.ts
@@ -15,17 +15,26 @@ import { CheckDom, CheckSelection, CheckSelectedFrames, CheckSelectedLayers } fr
 // @ts-expect-error
 import { generateNameUntilUnique, generateNameUseLast } from "SymbolNameGenerator";
 // @ts-expect-error
-import { Vector,Rectangle,Size } from "SAT";
-
+import { Vector, Rectangle, Size } from "SAT";
+// @ts-expect-error
+import JSFLConstants = require("JSFLConstants");
+// @ts-expect-error
+import { convertToKeyframesSafety } from "KeyFrameOperation";
+// @ts-expect-error
+import { setEaseCurveEx } from "EaseCurve";
+// @ts-expect-error
+import { playLoop,playLoopReverse } from "ElementAnim";
 
 // ===============Third Party======================
 import log = require("loglevel");
 // endregion import
 
+const { FRAME_1, FRAME_15, FRAME_30 } = JSFLConstants.Numerics.frame.frameList;
 
 // region doc
 var doc = fl.getDocumentDOM(); //文档
-if (!CheckDom(doc)) {//@ts-ignore
+if (!CheckDom(doc)) {
+    //@ts-ignore
     return;
 }
 
@@ -62,46 +71,111 @@ if (!CheckSelection(selection, "selectElement", "No limit")) {
 // endregion doc
 
 function getRect(radius: number) {
-    let elementPos=Vector.from(selection[0]);
     // left center  在原点，宽高为元素的宽高
-    let size = new Size(radius*2, radius*0.8);
+    let size = new Size(radius * 2, radius * 0.8);
 
-    let center = new Vector(size.width/2, 0);
+    let center = new Vector(size.width / 2, 0);
     let rect = Rectangle.fromCenter(center, size);
+    return rect;
 
-    return rect.addOffset(elementPos);
+    // let elementPos=Vector.from(selection[0]);
+    // return rect.addOffset(elementPos);
 }
 
+/**
+ * 生成这些offset的原因是：
+ * 如果想要让环绕的 物体 按照预定的轨迹去进行旋转的话，需要指定多个点， 椭圆  左边的中心点  向上偏移两个。 一个是椭圆，右边的中心点。 最后一个是   椭圆左边中心点   向下平移一个单位。
+ * 这样图形就会按照椭圆的轨迹进行旋转。
+ * 暂时不清楚。 为什么插件生成只有两个点 就能生成环绕效果，这里暂时用三个点。
+ * @param radius
+ */
+function getOffsets(radius: number) {
+    // 环绕矩形
+    let rect: Rectangle = getRect(radius);
+    let rightCenter = rect.getCorner("right center");
+    let leftCenter = rect.getCorner("left center");
+    // log.info("环绕矩形", rect, rightCenter, leftCenter);
+
+    let offset = new Vector(0, 2);
+    let top = leftCenter.clone().sub(offset);
+    let bottom = leftCenter.clone().add(offset);
+    log.info("环绕矩形", top, bottom);
+
+    let offsets = [top, rightCenter, bottom]; // 顺时针方向
+    // let offsets = [bottom, rightCenter, top];// 逆时针方向
+    // let offsets = [leftCenter,bottom]
+    return offsets;
+}
+
+let KEY_FRAMES: number[] = [FRAME_1, FRAME_15, FRAME_30];
+// let KEY_FRAMES: number[] = [FRAME_1, FRAME_30];
+
+let MOTION_GUIDE_LAYER_INDEX = 0;
+// 环绕层
+let AROUND_LAYER_INDEX = 1;
 
 function Main() {
+    let layers;
     // 选中：环绕飞行_内部--某个元件
     let RADIUS = 400; //环绕半径
 
     doc.enterEditMode("inPlace");
     let timeline = doc.getTimeline(); //时间轴
 
-
     let symbolName = generateNameUseLast("环绕飞行_飞行物");
     doc.convertToSymbol("graphic", symbolName, "center");
 
     // motionGuide 层
     {
-        let motionGuideLayerIndex = timeline.addMotionGuide();
+        MOTION_GUIDE_LAYER_INDEX = timeline.addMotionGuide();
         // log.info("添加动作导向", motionGuideLayerIndex,timeline.currentLayer);
 
         // 已经选中了 Motion Guide 层
-        var layers = timeline.layers; //图层
-        var motionGuideLayer = layers[motionGuideLayerIndex]; //动作导向层
+        layers = timeline.layers; //图层
+        var motionGuideLayer = layers[MOTION_GUIDE_LAYER_INDEX]; //动作导向层
 
         motionGuideLayer.name = "运动曲线";
 
         let rect = getRect(RADIUS);
-        doc.addNewOval(rect,true);
+        let elementPos = Vector.from(selection[0]);
+        let finalRect = rect.addOffset(elementPos);
+        // log.info("环绕矩形", rect);
+        doc.addNewOval(finalRect, true);
     }
 
-    // todo:insert keyframes
+    // insert keyframes
+    timeline.insertFrames(FRAME_30, true);
 
+    timeline.setSelectedLayers(AROUND_LAYER_INDEX);
+    convertToKeyframesSafety(timeline, KEY_FRAMES);
 
+    // 环绕层
+    {
+        layers = timeline.layers; //图层
+        var aroundLayer = layers[AROUND_LAYER_INDEX]; //环绕层
+
+        // 环绕矩形
+        let offsets = getOffsets(RADIUS);
+
+        // @ts-ignore
+        function setPosition(frame: number, position: Vector) {
+            timeline.setSelectedFrames([AROUND_LAYER_INDEX, frame, frame + 1]);
+
+            let FRAME_1_Element = aroundLayer.frames[frame].elements[0];
+            FRAME_1_Element.x = position.x;
+            FRAME_1_Element.y = position.y;
+        }
+
+        KEY_FRAMES.forEach((frame, index) => {
+            setPosition(frame, offsets[index]);
+        });
+
+        // 补间动画
+        setEaseCurveEx(timeline, KEY_FRAMES, "Classic Ease");
+    }
+
+    doc.exitEditMode();
+    playLoop();
 }
 
 Main();
